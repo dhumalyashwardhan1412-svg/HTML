c8c968e190968b46785d2ccb7d61a034dbf7415a
dhumalyashwardhan1412-svg <dhumalyashwardhan1412@gmail.com>
2026-06-04 15:44:13 +0530

    print

---
+++ batch-3/script.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+let a = 10;
+let b = 20;
+
+console.log(a+b);
+
+let name ="Shravan";
+
+console.log("My name is " + name);
+
+let age = 22;
+age = 100;
+console.log(age);
+
+let num1 = 10;
+let num2 = 20;
+
+console.log(num1 < num2);
+
+const PI = 3.14;
+
+console.log(PI);
